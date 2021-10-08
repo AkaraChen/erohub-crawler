18510e5dd43ac52c0edee93cc9340b872439471a
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-10-08 13:37:35 +0800

    测试应用机密

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+import sys
 from math import ceil
-
 import requests
 import json
 
+domain = sys.argv[1]
 publicDir = 'dist'
 pagesize = 12
 page = 1
@@ -31,17 +32,17 @@ def writeFile(name, content, category):
 
 
 # 实际文章数量比获取的少了八个，原因未知
-postCount = int(requestForData('https://api.erohub.org/api/count')['data']['counts']) - 8
+postCount = int(requestForData(domain + '/api/count')['data']['counts']) - 8
 pageCount = ceil(int(postCount / pagesize) + 2)
 print('本次下载共' + str(pageCount) + '页')
 
 while page <= pageCount:
-    postList = requestForData('https://api.erohub.org/api/posts?pageSize=' + str(pagesize) + '&page=' + str(page),
+    postList = requestForData(domain + '/api/posts?pageSize=' + str(pagesize) + '&page=' + str(page),
                               'origin')
     writeFile(str(page), str(postList), 'page')
     for item in json.loads(postList)['data']:
         cid = item['cid']
-        postContent = str(requestForData('https://api.erohub.org/api/post?cid=' + cid, 'origin'))
+        postContent = str(requestForData(domain + '/api/post?cid=' + cid, 'origin'))
         writeFile(cid, postContent, 'post')
         print('%s已经下载好了' % item['title'])
 
@@ -49,20 +50,23 @@ while page <= pageCount:
     page += 1
 
 webpage = 1
-webpageCount = ceil(int(requestForData('http://api.erohub.org/api/categoryList')['data'][0]['count']) / 12)
+webpageCount = ceil(int(requestForData(domain + '/api/categoryList')['data'][0]['count']) / 12)
 print(webpageCount)
 while webpage <= webpageCount:
-    WebPostList = requestForData('http://api.erohub.org/api/posts?category=2cy&pageSize=12&page=' + str(webpage),
+    WebPostList = requestForData(domain + '/api/posts?category=2cy&pageSize=12&page=' + str(webpage),
                                  'origin')
     writeFile(str(webpage), str(WebPostList), 'webpage')
     webpage += 1
 
+writeFile('manifest', str(webpage), 'webpage')
+
 mzituPage = 1
-mzituPageCount = ceil(int(requestForData('http://api.erohub.org/api/categoryList')['data'][1]['count']) / 12)
+mzituPageCount = ceil(int(requestForData(domain + '/api/categoryList')['data'][1]['count']) / 12)
 print(mzituPageCount)
 while mzituPage <= mzituPageCount:
-    mzituPostList = requestForData('http://api.erohub.org/api/posts?category=mzitu&pageSize=12&page=' + str(mzituPage),
+    mzituPostList = requestForData(domain + '/api/posts?category=mzitu&pageSize=12&page=' + str(mzituPage),
                                    'origin')
     writeFile(str(mzituPage), str(mzituPostList), 'mzitu')
     mzituPage += 1
 
+writeFile('manifest', str(mzituPage), 'mzitu')
