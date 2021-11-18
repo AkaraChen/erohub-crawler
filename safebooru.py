25f619695186f0d0e6a27420b687081dd38ce13e
AkaraChen <akarachen@outlook.com>
2021-11-18 19:12:39 +0800

    测试  safebooru

---
+++ safebooru.py
@@ -0,0 +1,30 @@
+import xmltodict
+import requests
+
+import os
+
+publicDir = 'safebooru'
+pagesize = 12
+page = 1
+
+os.system('rm -rf ' + publicDir)
+os.mkdir(publicDir)
+
+
+def requestForData(url):
+    r = requests.get(url)
+    return r.text
+
+
+def writeFile(name, content):
+    f = open(publicDir + '/' + name + '.json', 'w', encoding="utf-8")
+    f.write(content)
+    f.close()
+
+
+while page <= 100:
+    postList = xmltodict.parse(
+        requestForData('https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=12&pid=' + str(page)))
+    writeFile(str(page), str(postList))
+    print('第' + str(page) + '页下载好了')
+    page += 1
