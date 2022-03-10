3574f32120bffc65217acf19ef1f44526d451820
expliyh <expliyh@outlook.com>
2022-03-10 13:22:44 +0800

    即使没有tags也不会报错

--- expliyh_classes/rss.py
+++ expliyh_classes/rss.py
@@ -8,22 +8,29 @@ import requests
 class RSS:
     def __init__(self, link):
         rss = feedparser.parse(link)
-        print(rss)
-        articles = []
+        #        print(rss)
+        self.articles = []
         for row in rss['entries']:
             title = row['title']
             link = row['link']
             publish_time = row['published']
-            content = row['content']['value']
+            content = row['content'][0]['value']
             tags = []
             categories = []
-            for i in row['tags']:
-                if i['scheme'].find('/categories/') is not -1:
-                    categories.append(i['term'])
-                elif i['scheme'].find('/tags/') is not -1:
-                    tags.append(i['term'])
+            try:
+                for i in row['tags']:
+                    if i['scheme'].find('/categories/') != -1:
+                        categories.append(i['term'])
+                    elif i['scheme'].find('/tags/') != -1:
+                        tags.append(i['term'])
+            except KeyError:
+                pass
+
             page = requests.get(link)
             mark1 = page.text.find('<meta property="og:image" content="')
             start = mark1 + len('<meta property="og:image" content="')
             end = page.text.find('">', start)
             cover = page.text[start:end]
+            print(cover)
+            self.articles.append(article.Article(title, link, cover, categories, content))
+        print(self.articles[0].gen_dict())
